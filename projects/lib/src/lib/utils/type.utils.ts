0983648eaad4ef46eb59a035fc3bfdd732ba06c7
David van der Sluijs <david@pexlab.net>
2021-09-18 23:05:18 +0200

    feat(utils): add exclude type

--- projects/lib/src/lib/utils/type.utils.ts
+++ projects/lib/src/lib/utils/type.utils.ts
@@ -9,6 +9,8 @@ export type Replace<T, From, To> = T extends ( ...args: any[] ) => any ? T : {
     [ T[K], From ] extends [ From, T[K] ] ? To : Replace<T[K], From, To>
 }
 
+export type Exclude<T, U> = T extends U ? never : T;
+
 /**
  * Performs a deep merge of objects and returns new object. Does not modify
  * objects (immutable) and merges arrays via concatenation.
