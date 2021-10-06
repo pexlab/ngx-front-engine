9306d3f96098386bc075919cc7e68cb75b082972
David van der Sluijs <david@pexlab.net>
2021-10-06 19:29:06 +0200

    feat(showcase): add theme usage to speedometer

--- projects/docs/src/app/pages/showcase/speedometer/speedometer.component.ts
+++ projects/docs/src/app/pages/showcase/speedometer/speedometer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ComponentTheme, FeColorPalette, SpeedometerTheme } from '@pexlab/ngx-front-engine';
 
 @Component(
     {
@@ -12,6 +13,57 @@ export class SpeedometerComponent implements OnInit, OnDestroy {
     
     public speedometerValue = 0;
     private interval!: number;
+    
+    private speedometerNormalTheme: SpeedometerTheme = {
+        hud: FeColorPalette.Greyscale.SnowWhite,
+        
+        border: {
+            inner: FeColorPalette.Greyscale.SnowWhite,
+            outer: FeColorPalette.Cyan.AgalAquamarine
+        },
+        
+        indicator: {
+            gradientStart: FeColorPalette.Blue.VividCatalinaBlue,
+            gradientEnd  : FeColorPalette.Cyan.AgalAquamarine
+        },
+        
+        background: {
+            inner: FeColorPalette.Blue.PureBlue,
+            outer: FeColorPalette.Blue.Eclipse
+        }
+    };
+    
+    private speedometerRedTheme: SpeedometerTheme = {
+        
+        hud: FeColorPalette.Greyscale.SnowWhite,
+        
+        border: {
+            inner: FeColorPalette.Greyscale.SnowWhite,
+            outer: FeColorPalette.Red.Berry
+        },
+        
+        indicator: {
+            gradientStart: FeColorPalette.Red.DarkBlood,
+            gradientEnd  : FeColorPalette.Red.Berry
+        },
+        
+        background: {
+            inner: FeColorPalette.Red.NobleRed,
+            outer: '#380000'
+        }
+    };
+    
+    public determineTheme( value: number ): ComponentTheme<SpeedometerTheme> {
+        if ( value > 180 ) {
+            return {
+                palette: this.speedometerRedTheme
+            };
+        } else {
+            return {
+                palette: this.speedometerNormalTheme
+            };
+        }
+    }
     
     public ngOnInit(): void {
         
